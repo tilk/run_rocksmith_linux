687228de830dc7e27cf29eb60a6208693562377e
Marek Materzok <tilk@tilk.eu>
2021-01-13 14:03:34 +0100

    Add auto-detect for multiplayer

--- run_rocksmith.sh
+++ run_rocksmith.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 # Locate the Rocksmith directory
-#for ARG in "$@"; do
-#    if [[ `basename "$ARG"` = "Rocksmith2014.exe" ]]; then
-#        PATH=`dirname "$ARG"`
-#    fi
-#done
+for ARG in "$@"; do
+    if [[ `basename -- "$ARG"` = "Rocksmith2014.exe" ]]; then
+        RPATH=`dirname -- "$ARG"`
+    fi
+done
 
 # Find USB interfaces
 SOUND_DEVICE_REGEX="USB PnP Sound Device"
-CARDS=`arecord -l | grep "^card " | grep "\[$SOUND_DEVICE_REGEX\]" | sed "s/^card \([0-9]*\): .*$/\1/"`
+CARDS=(`arecord -l | grep "^card " | grep "\[$SOUND_DEVICE_REGEX\]" | sed "s/^card \([0-9]*\): .*$/\1/"`)
+
+# Update RS_ASIO.ini for multiplayer
+if [[ ${#CARDS[@]} > 1 ]]; then
+    ISEMI=";"; OSEMI=""
+else
+    ISEMI=""; OSEMI=";"
+fi
+sed -i~ "\$!N;/\\[Asio\\.Input\\.1\\]/s/\\n${ISEMI}Driver/\\n${OSEMI}Driver/;P;D" "$RPATH/RS_ASIO.ini"
 
 # Kill children processes on script exit
 trap 'kill $(jobs -p)' EXIT
 
 # Spawn JACK inputs
-for X in $CARDS; do
-    alsa_in -d hw:$X,0 -j USB$X &
+for X in ${CARDS[@]}; do
+    alsa_in -c 1 -d hw:$X,0 -j USB$X &
 done
 
 # Inhibit screen-saver
